65af37cd194ea66c9e8ae5496ebbde181b49d662
Taufik Budi S <tfkbudi@gmail.com>
2020-05-15 11:40:13 +0700

    update config & bump version

--- multichannel-widget/src/main/java/com/qiscus/qiscusmultichannel/MultichannelWidgetConfig.kt
+++ multichannel-widget/src/main/java/com/qiscus/qiscusmultichannel/MultichannelWidgetConfig.kt
@@ -10,6 +10,7 @@ import com.qiscus.qiscusmultichannel.util.MultichannelNotificationListener
 object MultichannelWidgetConfig {
     private var enableLog: Boolean = false
     private var isSessional: Boolean = false
+    private var enableNotification: Boolean = true
     var multichannelNotificationListener: MultichannelNotificationListener? = null
 
     fun setEnableLog(enableLog: Boolean) = apply { this.enableLog = enableLog }
@@ -18,5 +19,7 @@ object MultichannelWidgetConfig {
     fun setSessional(isSessional: Boolean) = apply { this.isSessional = isSessional }
     fun setNotificationListener(multichannelNotificationListener: MultichannelNotificationListener?) = apply { this.multichannelNotificationListener = multichannelNotificationListener }
     fun getNotificationListener() = multichannelNotificationListener
+    fun setEnableNotification(enableNotification: Boolean) = apply { this.enableNotification = enableNotification }
+    fun isEnableNotification() = enableNotification
 
 }
